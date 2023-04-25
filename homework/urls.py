de805c69897e766e96ee3a91806231bd78dcec2d
karola-b <karola.bralewska@gmail.com>
2023-04-25 18:20:34 +0200

    add calculator view

--- homework/urls.py
+++ homework/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from calculator import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('calculator/', views.calculator)
 ]
