c7e00f68efde32d2b68a507822f0639fb5b8c34f
karola-b <karola.bralewska@gmail.com>
2023-04-25 19:03:10 +0200

    create add view

---
+++ calculator/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('add/<int:num1>/<int:num2>/', views.calculator_add)
+]
--- calculator/views.py
+++ calculator/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render, HttpResponse
 
 def calculator(request):
     return HttpResponse("Welcome to my calculator")
+
+
+def calculator_add(request, num1, num2):
+    added_nums = num1 + num2
+    return render(
+        request,
+        'add.html',
+        context={
+            "num1": num1,
+            "num2": num2,
+            "sum": added_nums
+        }
+    )
